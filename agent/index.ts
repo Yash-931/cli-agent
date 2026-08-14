29083aa587b9a12f8dd9984a0a20170b5e30a81d
Yash-931 <yaggarwal931@gmail.com>
2026-08-14 14:30:33 +0530

    basic cli runner added

---
+++ agent/index.ts
@@ -0,0 +1,26 @@
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+console.log("Agent is running...");
+
+async function main() {
+  const rl = readline.createInterface({ input, output });
+
+  try {
+    while (true) {
+      const userInput = await rl.question("You: ");
+
+      if (userInput === "/exit") {
+        console.log("Goodbye!")
+        break;
+      }
+      console.log("Agent: " + userInput);
+    }
+  } catch (error) {
+    console.error("An error occurred " + error);
+  } finally {
+    rl.close();
+  }
+}
+
+main();
