e8c71bef27335f8e56cb4d944cdd4dd4ffe86d88
Yash-931 <yaggarwal931@gmail.com>
2026-08-26 20:11:49 +0530

    added the tooling in func

--- agent/ai.ts
+++ agent/ai.ts
@@ -1,4 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
+import { calculator } from "./tools";
 
 const ai = new GoogleGenAI({
   vertexai: true,
@@ -6,15 +7,51 @@ const ai = new GoogleGenAI({
   project: process.env.GCP_PROJECT,
 });
 
+const calculatorDeclaration = {
+  name: "calculator",
+  description: "Calculate a mathematical expression",
+  parameters: {
+    type: "OBJECT",
+    properties: {
+      a: {
+        type: "NUMBER",
+        description: "The first number",
+      },
+
+      b: {
+        type: "NUMBER",
+        description: "The second number",
+      },
+
+      op: {
+        type: "STRING",
+        description: "The mathematical operation to perform",
+        enum: ["add", "subtract", "multiply", "divide"],
+      },
+    },
+  },
+};
+
 export async function responseGeneration(prompt: string) {
-  const stream = await ai.models.generateContentStream({
+  const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
     contents: prompt,
+    config: {
+      tools: [
+        {
+          functionDeclarations: [calculatorDeclaration],
+        },
+      ],
+    },
   });
 
-  for await (const chunk of stream) {
-    process.stdout.write(chunk.text ?? "")
+  const calls = response.functionCalls;
+
+  if(calls) {
+    const toolResponse = calculator(calls[0]?.args.a, calls[0]?.args.b, calls[0]?.args.op)
   }
 
-  process.stdout.write("\n")
+  process.stdout.write("\n");
 }
+
+await responseGeneration("What is 25 + 15")
---
+++ agent/tools.ts
@@ -0,0 +1,24 @@
+export function calculator(a: number, b: number, op: string) {
+    if(op === "add") {
+        return a+b;
+    }
+
+    else if(op === "multiply"){
+        return a*b;
+    }
+
+    else if(op === "subtract"){
+        return a-b;
+    }
+
+    else if(op === "divide") {
+        if(b === 0){
+            return "Cannot divide by 0";
+        }
+        return a/b;
+    }
+
+    else {
+        return "Invalid operation entered by the user"
+    }
+}
